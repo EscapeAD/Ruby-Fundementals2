5b1b99e0446a3acf8bb99861611fb30a22a85990
Adam Tse <mr.adam.tse@gmail.com>
2016-08-10 14:39:07 -0400

    Added bonus for excersize 7

--- excersize7.rb
+++ excersize7.rb
@@ -7,19 +7,32 @@ students = {
 def rooms(room)
   room.each do | x , y |
   puts "room with #{x} has #{y} students"
+  end
 end
+
+def rooms2(room)
+  room.each do | x , y |
+  y * 1.05
+  y.floor
+  end
+end
+
+def total(room)
+  i = 0
+  room.each do | x , y |
+  i += y
+  end
+  puts "Total Students in all cohoerts #{i}"
 end
 
 rooms(students)
-
 students[:cohert4] = 43
-
 students.keys
 
-expanded = students.map do | x, y|
-  x
-  i = y * 1.15
-  i.floor
-end
+puts ""
 
-rooms(expanded)
+rooms2(students)
+rooms(students)
+
+puts "bonus time"
+total(students)
